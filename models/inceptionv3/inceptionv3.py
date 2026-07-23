34c7621dacbc04c22fa028082bb36e2556f66f8a
cyrilmarat <cyril.marat@gmail.com>
2026-07-23 10:44:48 +0200

    .keras

--- models/inceptionv3/inceptionv3.py
+++ models/inceptionv3/inceptionv3.py
@@ -206,27 +206,52 @@ model.save('inceptionv3_finetuned.keras')
 
 
 # %%
+
+plt.figure()
+plt.xlabel('Epochs')
+plt.ylabel('f1_score')
+
+# Courbe de la précision sur l'échantillon d'entrainement (phase de fine-tuning)
+plt.plot(
+         model_history.history['f1_score'],
+         label='Training f1_score (head)',
+         color='blue')
+
+# Courbe de la précision sur l'échantillon de validation (phase de fine-tuning)
+plt.plot(
+         model_history.history['val_f1_score'],
+         label='Validation f1_score (head)',
+         color='red')
+
+# Affichage de la légende
+plt.legend()
+
+plt.savefig('inceptionv3_head.png')
+
+
 # Labels des axes
+plt.figure()
 plt.xlabel('Epochs')
 plt.ylabel('f1_score')
 
 # Courbe de la précision sur l'échantillon d'entrainement (phase de fine-tuning)
 plt.plot(
          fine_tune_history.history['f1_score'],
-         label='Training f1_score',
+         label='Training f1_score (fine tuned)',
          color='blue')
 
 # Courbe de la précision sur l'échantillon de validation (phase de fine-tuning)
 plt.plot(
          fine_tune_history.history['val_f1_score'],
-         label='Validation f1_score',
+         label='Validation f1_score (fine tuned)',
          color='red')
 
 # Affichage de la légende
 plt.legend()
 
-plt.savefig('inceptionv3.png')
-np.save('inceptionv3.npy',model_history.history)
+plt.savefig('inceptionv3_finetune.png')
+np.save('inceptionv3_head.npy', model_history.history)
+np.save('inceptionv3_finetuned.npy', fine_tune_history.history)
 
 # Affichage de la figure
 plt.show()
