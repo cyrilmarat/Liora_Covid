538a057ebbf41f0be6152d4aaa2bfa2b0e162033
cyrilmarat <cyril.marat@gmail.com>
2026-08-03 23:27:07 +0200

    streamlit

--- src/streamlit/app.py
+++ src/streamlit/app.py
@@ -178,12 +178,104 @@ if page == pages[1] :
         plt.title(f"Classe : {classe}")
 
     st.pyplot(fig)
+    
+    st.write("### Distribution des pixels par classe")
+    col1, col2 , col3, col4= st.columns(4)
+    with col1:
+        st.image("hist_intensite_1.png", caption="Sans masque")
+
+    with col2:
+        st.image("hist_intensite_2_masques.png", caption="Avec masque")
+
+    with col3:
+        st.image("hist_intensite_3.png", caption="Sans masque")
+
+    with col4:
+        st.image("hist_intensite_4_masques.png", caption="Avec masque")
+
+    st.write("L'analyse de la luminosité des images montre des différences entre les différentes classes. La moyenne des pixels traduit le niveau global de luminosité d'une radiographie tandis que l'écart-type renseigne sur l'hétérogénité des nivveaux de gris. La classe Covid présente les intensités les plus élevées et ses valeurs sont plus dispersées tandis que les autres classes présentent des valeurs assez proches. L'apllication des masques modifient ces indicateurs en diminuant l'intensité lumineuse et en resserrant la distribution. Les différences entre les classes sont moins marquées.")
+
+
+    st.write("### Analyse en composantes principales")
+
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        st.image("ACP_1.png")
+
+    with col2:
+        st.image("ACP_3.png")
+
+    with col3:
+        st.image("ACP_4.png")
+
+    st.write("Une analyse en composantes principales (ACP) sur le jeu de données est réalisée afin de visualiser la structure des données, repérer d'éventuels outliers et évaluer la séparation des différentes classes.")
+    st.write("La forte superposition des classes suggère que les différences observées sont dues à des motifs radiologiques complexes et locaux. Cela justifie le recours a des modèles d'apprentissage capables d'extraire les caractéristiques discriminantes spécifique à chaque classe.")
+
+
+    st.write("### Outliers")
+
+
+    st.write("Pour chaque classe, les images dont la distance quadratique moyenne (MSE) par rapport à l'image moyenne de leur classe dépasse un seuil ont été identifiées comme atypiques :")
+    st.write("- Normal : 11 outliers (MSE > 2500)")
+    st.write("- COVID : 34 outliers (MSE > 4 000)")
+    st.write("- Lung Opacity : 20 outliers (MSE > 4 000)")
+    st.write("- Viral Pneumonia : 32 outliers (MSE > 3 000)")
+    st.write("Les distances sont calculées en travaillant sur une image redimensionnée à 50x50 pixels")
+
 
 # --------------------------------------------------------------------------- #
 # Feature Extraction
 # --------------------------------------------------------------------------- #
 if page == pages[2] : 
     st.write("### Feature Extraction")
+    
+    features = pd.DataFrame({"Features":[
+                "pixel_mean/pixel_std",
+                "lum_interieur_masque / lum_exterieur_masque",
+                "Surface_masque",
+                "Variance laplacien"
+            ],
+            "Description":["Luminosité moyenne et dispersion des pixels sur l'image totale",
+            "Luminosité moyenne dans et hors de la zone pulmonaire",
+            "Proportion de pixels pulmonaires dans l'image",
+            "Variance du Laplacien, indicateur de netteté de l'image"]
+            })
+    
+    strategies_analyse=pd.DataFrame({"Stratégie":["Data augmentation", "Class weigths"],
+                                        "Description":["Via ImageDataGenerator de Keras, appliquée uniquement sur le jeu d'entraînement. Elle permet d'augmenter artificiellement le nombre d'images des classes minoritaires par transformations géométriques (rotation, flip horizontal, zoom)",
+                                        "Pour les modèles sensibles au déséquilibre (CNN, Random Forest), un poids inversement proportionnel à la fréquence de chaque classe sera appliqué dans la fonction de perte, forçant le modèle à accorder plus d'importance aux classes rares comme Viral Pneumonia."]
+                                        })
+    
+    deduplication=pd.DataFrame({"Classe":["Covid", "Normal", "Lung Opacity", "Viral Pneumonia"],
+                                    "Doublons":[1, 223, 0, 7],
+                                    "outliers":[23, 55, 24, 0]})
+    
+    st.write("### Preprocessing")
+    
+    st.write("Pour répondre aux constats dressés lors de l'étape d'exploration, nous avons défini le pipeline de preprocessing ci-dessous.")
+    
+    st.image("pipeline_preprocessing.png", caption="Pipeline preprocessing")
+    
+    st.write("Cette étape comprend les processus de nettoyage, de traitement des images, de séparation et enfin d'augmantation sur le jeu d'entraînement uniquement.")
+    
+    st.write("Cette stratégie a été adoptée pour fiabiliser le jeu de données (dé-duplication), supprimer le fond (masques), harmoniser les caractéristiques des images (normalisation) et augmenter la variabilité des données (augmentation)")
+    
+    st.write("L'étape d'exploration a permis de repérer 231 doublons et 102 outliers")
+    st.table(deduplication)
+    
+    st.write("### Génération d'un dataset des caractéristiques des images")
+    
+    st.write("Un datase tabulaire a été généré en vue de l'entraînement des modèles de machine learning. Il contient l'ensemble des features des images traitées :")
+    st.table(features)
+    
+    st.write("Le jeu de données ainsi constitué compte 20 835 images réparties sur les 4 classes.")
+    
+    st.write("### Gestion des déséquilibres")
+    
+    st.write("Deux stratégies ont été définies pour prendre en compte le déséquilibre des classes constaté lors de l'étape d'analyse :")
+    st.table(strategies_analyse)
+    
 
 # --------------------------------------------------------------------------- #
 # Modèle SVM
